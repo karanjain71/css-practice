23ca5469c4d2bb881fa4e2ab886fef70058e209b
Karan Jain <karan.jain@sap.com>
2022-03-05 16:39:24 +0530

    First Commit

---
+++ ecommerce-product-page-main/script.js
@@ -0,0 +1,12 @@
+const plusSign = document.querySelector('.icon-plus')
+// plusSign.addEventListener('click',(event) => {
+//     console.log('Here we are')
+// })
+
+console.log('This is the thng')
+
+const addtoCart = () => {
+    console.log('Here we are adding to the cart')
+}
+console.log(document.querySelector(".button-group"),'this')
+document.addEventListener('click',addtoCart)
